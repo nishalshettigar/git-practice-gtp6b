fffc4c02f8c9b41fef27d17f0b18241d322b980e
nishalshettigar <nishalshettigar@gmail.com>
2018-01-11 17:44:54 +0530

    adding some comments

--- HelloGit.java
+++ HelloGit.java
@@ -1,8 +1,11 @@
 public class HelloGit {
 
+	//Main method
 	public static void main(String[] args) {
+		//This creates a 'MessageDisplayer' object.
 		MessageDisplayer messageDisplayer = new MessageDisplayer();
 
+		//This displays hello world message.
 		messageDisplayer.displayMessage("Hello github!");
 	}
 }
